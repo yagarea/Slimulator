6d3a72aba59b7915c421303c180192e201e15bd0
Jan <j.cerny.zdar@gmail.com>
2021-05-05 23:35:29 +0200

    init Launcher

---
+++ Launcher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Slimulator {
+    static class Launcher {
+        static void Main(string[] args) {
+            Console.WriteLine($"Loading file {args[0]}");
+            Space space = new Space(args[0]);
+            Simulation sim = new Simulation(space, "eeej", @"outputvideo.mp4");
+            for(int i = 0; i < 300; i++) sim.tick();
+            sim.End();
+        }
+    }
+}
