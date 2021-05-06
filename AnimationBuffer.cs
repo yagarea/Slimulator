6f095c05c6a7cc281ed4791adba79d6377e8dbf0
Jan <j.cerny.zdar@gmail.com>
2021-05-06 20:05:56 +0200

    bond animation buffer to ffmpeg

--- AnimationBuffer.cs
+++ AnimationBuffer.cs
@@ -1,23 +1,35 @@
+using System;
 using System.Drawing;
+using System.Drawing.Imaging;
+using FFMediaToolkit;
 using FFMediaToolkit.Encoding;
 using FFMediaToolkit.Graphics;
 
 namespace Slimulator {
     public class AnimationBuffer {
-        private MediaOutput buffer;
-        private ImageConverter converter;
+        private readonly MediaOutput buffer;
+        private readonly ImageConverter converter;
 
         public AnimationBuffer(string videoPath, int height, int width, int frameRate) {
+            FFmpegLoader.FFmpegPath = "/usr/lib/";
             buffer = MediaBuilder.CreateContainer(videoPath).WithVideo(new VideoEncoderSettings(width: width,
                 height: height, framerate: frameRate,
                 codec: VideoCodec.H264)
             ).Create();
             converter = new ImageConverter();
         }
+        
+        private static ImageData FrameToImageData(Bitmap bitmap) {
+            var rect = new Rectangle(System.Drawing.Point.Empty, bitmap.Size);
+            var bitLock = bitmap.LockBits(rect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+
+            var bitmapData = ImageData.FromPointer(bitLock.Scan0, ImagePixelFormat.Bgr24, bitmap.Size);
+            bitmap.UnlockBits(bitLock);
+            return bitmapData;
+        }
 
         public void AddFrame(Bitmap frame) {
-            buffer.Video.AddFrame(ImageData.FromArray((byte[]) converter.ConvertTo(frame, typeof(byte[])),
-                ImagePixelFormat.Argb32, frame.Width,frame.Height));
+            buffer.Video.AddFrame(FrameToImageData(frame));
         }
 
         public void Export() {
