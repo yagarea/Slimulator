09873d805511a15f9f94f860d1ee13dc3fc7add1
Jan <j.cerny.zdar@gmail.com>
2021-05-06 20:59:12 +0200

    test launcher

--- Launcher.cs
+++ Launcher.cs
@@ -1,13 +1,14 @@
 ﻿using System;
+using System.Drawing.Imaging;
 
 namespace Slimulator {
     static class Launcher {
         static void Main(string[] args) {
             Console.WriteLine($"Loading file {args[0]}");
             Space space = new Space(args[0]);
-            Simulation sim = new Simulation(space, "eeej", @"outputvideo.mp4");
-            for(int i = 0; i < 300; i++) sim.tick();
-            sim.End();
+            Simulation sim = new Simulation(space, "eeej", @"/home/john/Projects/Slimulator/outputvideo.mp4");
+            for(int i = 0; i < 3000; i++) sim.Tick();
+            sim.End(true);
         }
     }
 }
