120bb904bd1adddc421e84e8c13d64fcd983b06b
Jan <j.cerny.zdar@gmail.com>
2021-05-06 20:21:32 +0200

    add polarity to point

--- Point.cs
+++ Point.cs
@@ -1,24 +1,42 @@
 namespace Slimulator {
-    internal enum PointType {
+    public enum PointType {
         Wall,
         Space,
         Slime,
         Food
     };
 
-    internal class Point {
+    public enum PolarityType {
+        Repulsive,
+        Attractive,
+        Unreachable
+    };
+
+    public class Point {
         private PointType _type;
+        private PolarityType polarity;
         private int _slimeAffinity;
 
         public Point(PointType pt) {
             _type = pt;
-            _slimeAffinity = pt switch {
-                PointType.Space => 10,
-                PointType.Wall => -1,
-                PointType.Food => 1000,
-                PointType.Slime => 1,
-                _ => _slimeAffinity
-            };
+            switch (pt) {
+                case PointType.Space:
+                    _slimeAffinity = 10;
+                    polarity = PolarityType.Attractive;
+                    break;
+                case PointType.Wall:
+                    _slimeAffinity = -1;
+                    polarity = PolarityType.Unreachable;
+                    break;
+                case PointType.Food:
+                    _slimeAffinity = 1000;
+                    polarity = PolarityType.Attractive;
+                    break;
+                case PointType.Slime:
+                    _slimeAffinity = 1;
+                    polarity = PolarityType.Repulsive;
+                    break;
+            }
         }
 
         public Point(PointType pt, int lop) {
