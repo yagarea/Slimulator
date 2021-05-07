8b40d2bcd4a13639a84a0adbc07406913f3fd6f3
Jan <j.cerny.zdar@gmail.com>
2021-05-07 18:34:31 +0200

    clean up Point class

--- Point.cs
+++ Point.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Threading;
+
 namespace Slimulator {
     public enum PointType {
         Wall,
@@ -6,44 +9,52 @@ namespace Slimulator {
         Food
     };
 
-    public enum PolarityType {
-        Repulsive,
-        Attractive,
-        Unreachable
-    };
-
     public class Point {
         private PointType _type;
-        private PolarityType polarity;
         private int _slimeAffinity;
+        private int x;
 
-        public Point(PointType pt) {
+        public PointType Type => _type;
+
+        public int SlimeAffinity => _slimeAffinity;
+
+        public int X => x;
+
+        public int Y => y;
+
+        private int y;
+
+        public Point(int x, int y,PointType pt) {
             _type = pt;
+            this.x = x;
+            this.y = y;
             switch (pt) {
                 case PointType.Space:
                     _slimeAffinity = 10;
-                    polarity = PolarityType.Attractive;
                     break;
                 case PointType.Wall:
                     _slimeAffinity = -1;
-                    polarity = PolarityType.Unreachable;
                     break;
                 case PointType.Food:
                     _slimeAffinity = 1000;
-                    polarity = PolarityType.Attractive;
                     break;
                 case PointType.Slime:
                     _slimeAffinity = 1;
-                    polarity = PolarityType.Repulsive;
                     break;
             }
         }
 
-        public Point(PointType pt, int lop) {
+        public Point(int x, int y, PointType pt, int lop) {
+            this.x = x;
+            this.y = y;
             _type = pt;
             _slimeAffinity = lop;
         }
 
+        public void SetType(PointType pt) {
+            this._type = pt;
+        }
+        
         public int GetSlimeAffinity() => _slimeAffinity;
         public new PointType GetType() => _type;
         
