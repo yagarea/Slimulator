c5c7fd5b7b8e011e9f5d2b766322e64c69f242cf
Jan <j.cerny.zdar@gmail.com>
2021-05-07 18:36:48 +0200

    first iteration of simulation

--- Simulation.cs
+++ Simulation.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing.Imaging;
+using System.Linq;
 
 namespace Slimulator {
     public class Simulation {
-        private Random random;
+        private Random randomizer;
         private Space space;
         private AnimationBuffer _animationBuffer;
         private string outputVideoPath;
@@ -13,26 +15,33 @@ namespace Slimulator {
         public Simulation(Space space, string seed, string outputVideoPath) {
             this.outputVideoPath = outputVideoPath;
             this.space = space;
-            random = new Random(seed.GetHashCode());
-            _animationBuffer = new AnimationBuffer(outputVideoPath, space.Height, space.Width, 30);
+            randomizer = new Random(seed.GetHashCode());
+            _animationBuffer = new AnimationBuffer(outputVideoPath, space.Height, space.Width, 60);
             _animationBuffer.AddFrame(space.ExportBitmap());
             tickCount = 0;
         }
 
         public void Tick() {
+            PickRandomPoint(Slime.FindAllPossiblePlacesToMove(space, Slime.FindAllSlime(space)))
+                .SetType(PointType.Slime);
+            
             _animationBuffer.AddFrame(space.ExportBitmap());
-            //space.ExportBitmap().Save(tickCount.ToString() + ".png", ImageFormat.Png);
-            //space.TextLog();
             tickCount++;
+            Console.WriteLine($"Frame: {tickCount}");
         }
 
-        public void End(bool play=false) {
+        public void End(bool play = false) {
             _animationBuffer.Export();
             if (play) {
-                ProcessStartInfo startInfo = new ProcessStartInfo() { FileName = "vlc", Arguments = outputVideoPath, }; 
-                Process proc = new Process() { StartInfo = startInfo, };
+                ProcessStartInfo startInfo = new ProcessStartInfo() {FileName = "vlc", Arguments = outputVideoPath,};
+                Process proc = new Process() {StartInfo = startInfo,};
                 proc.Start();
             }
+        }
+
+        private Point PickRandomPoint(HashSet<Point> points) {
+            Point[] pointArray = points.ToArray();
+            return pointArray[randomizer.Next(pointArray.Length)];
         }
     }
 }
