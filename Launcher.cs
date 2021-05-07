d036a662ecd58ed01b88dfdbb2ae5ef0a7e09fe1
Jan <j.cerny.zdar@gmail.com>
2021-05-07 18:37:59 +0200

    implement basic launcher

--- Launcher.cs
+++ Launcher.cs
@@ -4,10 +4,10 @@ using System.Drawing.Imaging;
 namespace Slimulator {
     static class Launcher {
         static void Main(string[] args) {
-            Console.WriteLine($"Loading file {args[0]}");
-            Space space = new Space(args[0]);
-            Simulation sim = new Simulation(space, "eeej", @"/home/john/Projects/Slimulator/outputvideo.mp4");
-            for(int i = 0; i < 3000; i++) sim.Tick();
+            String[] testFiles = new[] {"/home/john/Projects/Slimulator/test_mazes/basic-maze.png", "/home/john/Projects/Slimulator/test_mazes/small-maze-food2.png"};
+            Space space = new Space(testFiles[0]);
+            Simulation sim = new Simulation(space, "eeeeej", @"/home/john/Projects/Slimulator/outputvideo.mp4");
+            for (int i = 0; i < 10000; i++) sim.Tick();
             sim.End(true);
         }
     }
