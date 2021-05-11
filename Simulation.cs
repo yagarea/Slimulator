e3e050f8732645dffd2f0e95791ac47f7c5adce1
Jan <j.cerny.zdar@gmail.com>
2021-05-11 15:14:45 +0200

    complete rework simulation infrastructure

--- Simulation.cs
+++ Simulation.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 
 namespace Slimulator {
@@ -11,31 +11,56 @@ namespace Slimulator {
         private AnimationBuffer _animationBuffer;
         private string outputVideoPath;
         private int tickCount;
+        private int ticksPerFrame;
+        private int simTicks;
 
-        public Simulation(Space space, string seed, string outputVideoPath) {
+        public Simulation(Space space, string outputVideoPath, int simTicks, string seed = "HlenkaHelenka",
+            int ticksPerFrame = 3, int frameRate = 60) {
             this.outputVideoPath = outputVideoPath;
             this.space = space;
+            this.simTicks = simTicks;
             randomizer = new Random(seed.GetHashCode());
-            _animationBuffer = new AnimationBuffer(outputVideoPath, space.Height, space.Width, 60);
+            _animationBuffer = new AnimationBuffer(outputVideoPath, space.Height, space.Width, frameRate);
             _animationBuffer.AddFrame(space.ExportBitmap());
             tickCount = 0;
+            this.ticksPerFrame = ticksPerFrame;
+            Console.WriteLine($"Output file: {outputVideoPath}");
+            Console.WriteLine(
+                $"      Specs: FPS: {frameRate} TFC:{simTicks / ticksPerFrame} TimeSpan:{(simTicks / ticksPerFrame) / frameRate}");
         }
 
-        public void Tick() {
-            PickRandomPoint(Slime.FindAllPossiblePlacesToMove(space, Slime.FindAllSlime(space)))
+        private void Tick() {
+            HashSet<Point> currentSlime = Slime.FindAllSlime(space);
+            PickRandomPoint(Slime.FindAllPossiblePlacesToMove(space, currentSlime))
                 .SetType(PointType.Slime);
-            
-            _animationBuffer.AddFrame(space.ExportBitmap());
+            PickRandomPoint(Slime.FindAllPossibleSlimesToPerish(space, currentSlime)).SetType(PointType.ExploredSpace);
+            if (tickCount % ticksPerFrame == 0) _animationBuffer.AddFrame(space.ExportBitmap());
             tickCount++;
-            Console.WriteLine($"Frame: {tickCount}");
+            space.GetOlder();
+            Console.SetCursorPosition(0, Console.CursorTop -1);
+            Console.WriteLine($"Tick: {tickCount}");
+        }
+
+        public int Start() {
+            Console.WriteLine("Simulation started\n");
+            while (tickCount < simTicks) {
+                Tick();
+            }
+
+            return tickCount;
         }
 
         public void End(bool play = false) {
+            _animationBuffer.AddFrame(space.ExportBitmap());
             _animationBuffer.Export();
             if (play) {
-                ProcessStartInfo startInfo = new ProcessStartInfo() {FileName = "vlc", Arguments = outputVideoPath,};
-                Process proc = new Process() {StartInfo = startInfo,};
-                proc.Start();
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.Arguments = Path.GetFullPath(outputVideoPath);
+                startInfo.FileName = "/usr/bin/vlc";
+                startInfo.CreateNoWindow = true;
+                using (Process p = Process.Start(startInfo)) {
+                    p.WaitForExit();
+                }
             }
         }
 
