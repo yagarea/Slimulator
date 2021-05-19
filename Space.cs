a1141d65a70561ad8cbb65ed32b97c7f586f34e5
Jan <cernyj27@gitlab.mff.cuni.cz>
2021-05-19 17:12:58 +0200

    implement distance in space

--- Space.cs
+++ Space.cs
@@ -11,14 +11,14 @@ namespace Slimulator {
         public int Width => _width;
 
         private readonly int _width;
-        private readonly Point[][] points;
+        private readonly Point[,] _points;
 
         private PointType TypeOfColor(Color c) {
             return c.R switch {
                 0 when c.G == 0 && c.B == 0 => PointType.Wall,
                 255 when c.G == 0 && c.B == 0 => PointType.Food,
                 255 when c.G == 255 && c.B == 0 => PointType.Slime,
-                _ => PointType.UnexploredSpace
+                _ => PointType.Space
             };
         }
 
@@ -36,18 +36,18 @@ namespace Slimulator {
             Bitmap bm = new Bitmap(inputPath);
             _height = bm.Height;
             _width = bm.Width;
-            points = new Point[_width][];
+            _points = new Point[_width, _height];
             for (int x = 0; x < _width; x++) {
-                points[x] = new Point[_height];
-                for (int y = 0; y < _height; y++) points[x][y] = new Point(x, y, TypeOfColor(bm.GetPixel(x, y)));
+                for (int y = 0; y < _height; y++) _points[x, y] = new Point(x, y, TypeOfColor(bm.GetPixel(x, y)));
             }
+
             Console.WriteLine($"Space constructed [{_height}x{_width}]");
         }
 
         public Bitmap ExportBitmap() {
             Bitmap outbm = new Bitmap(_height, _width);
             for (int x = 0; x < _width; x++) {
-                for (int y = 0; y < _height; y++) outbm.SetPixel(x, y, ColorOfType(points[x][y].GetType()));
+                for (int y = 0; y < _height; y++) outbm.SetPixel(x, y, ColorOfType(_points[x, y].GetType()));
             }
 
             return outbm;
@@ -59,12 +59,12 @@ namespace Slimulator {
 
         public Point GetPoint(int x, int y) {
             if (IsInBound(x, y)) return new Point(x, y, PointType.Wall);
-            return points[x][y];
+            return _points[x, y];
         }
 
         public PointType GetPointType(int x, int y) {
             if (IsInBound(x, y)) return PointType.Wall;
-            return points[x][y].GetType();
+            return _points[x, y].GetType();
         }
 
         public HashSet<Point> GetAccessibleNeighbours(int x, int y) {
@@ -82,9 +82,9 @@ namespace Slimulator {
         }
 
         public bool IsInMiddleOfSlime(int x, int y) {
-            foreach (int shift in new []{-1, 1}){ 
+            foreach (int shift in new[] {-1, 1}) {
                 if (GetPointType(x + shift, y) != PointType.Slime ||
-                 GetPointType(x, y + shift) != PointType.Slime) {
+                    GetPointType(x, y + shift) != PointType.Slime) {
                     return false;
                 }
             }
@@ -93,14 +93,16 @@ namespace Slimulator {
         }
 
         public void GetOlder() {
-            for (int x = 0; x < _width; x++) for (int y = 0; y < _height; y++) points[x][y].GetOlder();
+            for (int x = 0; x < _width; x++)
+            for (int y = 0; y < _height; y++)
+                _points[x, y].GetOlder();
         }
 
         public void TextLog() {
             Console.Write('\n');
             for (int x = 0; x < _width; x++) {
                 for (int y = 0; y < _height; y++) {
-                    switch (points[x][y].GetType()) {
+                    switch (_points[x, y].GetType()) {
                         case PointType.Wall:
                             Console.Write("#");
                             break;
@@ -122,9 +124,25 @@ namespace Slimulator {
             }
         }
 
+        private double DistanceOf(Point a, Point b) {
+            return Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2));
+        }
+
+        public int OccurrenceInRadius(Point p, PointType searchedPointType, int radius) {
+            int occurance = 0;
+            radius = Math.Abs(radius);
+            for (int xShift = -radius; xShift <= radius; xShift++) {
+                for (int yShift = -radius; yShift <= radius; yShift++) {
+                    if (DistanceOf(p, GetPoint(p.X + xShift, p.Y + yShift)) <= radius &&
+                        GetPointType(p.X + xShift, p.Y + yShift) == searchedPointType) occurance++;
+                }
+            }
+            return occurance;
+        }
+
         public void AffinityLog() {
             for (int x = 0; x < _width; x++) {
-                for (int y = 0; y < _height; y++) Console.Write(points[x][y].GetSlimeAffinity() + " ");
+                for (int y = 0; y < _height; y++) Console.Write(_points[x, y].GetSlimeAffinity() + " ");
                 Console.Write('\n');
             }
         }
