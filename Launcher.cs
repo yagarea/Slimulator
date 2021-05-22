77b5b26cb97f256e8dd92f76bba54976475b20b7
Jan <j.cerny.zdar@gmail.com>
2021-05-22 16:23:06 +0200

    update launcher

--- Launcher.cs
+++ Launcher.cs
@@ -1,22 +1,18 @@
 ﻿using System;
-using System.Drawing.Imaging;
 
 namespace Slimulator {
     static class Launcher {
         static void Main(string[] args) {
-            String[] testFiles = new[] {
+            string[] testFiles = {
                 "/home/john/Projects/Slimulator/test_mazes/maze100-food2.png", 
                 "/home/john/Projects/Slimulator/test_mazes/maze500-food2.png",
                 "/home/john/Projects/Slimulator/test_mazes/maze1000-food2.png"
             };
-            Space space = new Space(testFiles[1]);
-            Simulation sim = new Simulation(space, 
-                @"/home/john/Projects/Slimulator/outputvideo.mp4",
-                simTicks:50000,
-                ticksPerFrame: 20,
-                frameRate: 60);
-            sim.Start();
-            sim.End(true);
+            Simulation sim = new Simulation(testFiles[0],
+                @$"/home/john/Projects/Slimulator/export/SlimulatorVideo-{DateTime.Now:HH-mm-ss}.mp4",
+                SimulationSettings.Fast());
+            Console.WriteLine(sim.Start());
+            sim.End(false);
         }
     }
 }
