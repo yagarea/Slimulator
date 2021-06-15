ad772233f1f819d70d4db04d8864f56865c6b904
Jan <j.cerny.zdar@gmail.com>
2021-06-15 22:57:38 +0200

    update Launcher with wizard

--- Launcher.cs
+++ Launcher.cs
@@ -3,16 +3,9 @@
 namespace Slimulator {
     static class Launcher {
         static void Main(string[] args) {
-            string[] testFiles = {
-                "/home/john/Projects/Slimulator/test_mazes/maze100-food2.png", 
-                "/home/john/Projects/Slimulator/test_mazes/maze500-food2.png",
-                "/home/john/Projects/Slimulator/test_mazes/maze1000-food2.png"
-            };
-            Simulation sim = new Simulation(testFiles[0],
-                @$"/home/john/Projects/Slimulator/export/SlimulatorVideo-{DateTime.Now:HH-mm-ss}.mp4",
-                SimulationSettings.Fast());
+            Simulation sim = Wizard.setup();
             Console.WriteLine(sim.Start());
-            sim.End(false);
+            sim.End(true);
         }
     }
 }
