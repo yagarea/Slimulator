fc55583ef800ccabe8c649a1b2edc335c6f9cb68
Jan <j.cerny.zdar@gmail.com>
2021-06-16 00:30:14 +0200

    refactor simulation settings

--- SimulationSettings.cs
+++ SimulationSettings.cs
@@ -1,16 +1,5 @@
 namespace Slimulator {
     public class SimulationSettings {
-        private int _totalCountOfSimulationTicks;
-        private int _ticksPerFrame;
-        private int _frameRate;
-
-        private string _seed;
-        private int _threadCount;
-
-        private int _slimeAffinityRadius;
-        private int _slimeOccurenceAffinityMultiplier;
-        private int _slimeTimeAffinityMultiplier;
-
         public SimulationSettings(
             int totalCountOfSimulationTicks = 70_000,
             int ticksPerFrame = 20,
@@ -20,24 +9,24 @@ namespace Slimulator {
             int slimeAffinityRadius = 4,
             int slimeOccurenceAffinityMultiplier = 1,
             int slimeTimeAffinityMultiplier = 10) {
-            _totalCountOfSimulationTicks = totalCountOfSimulationTicks;
-            _ticksPerFrame = ticksPerFrame;
-            _frameRate = frameRate;
-            _seed = seed;
-            _threadCount = threadCount;
-            _slimeAffinityRadius = slimeAffinityRadius;
-            _slimeOccurenceAffinityMultiplier = slimeOccurenceAffinityMultiplier;
-            _slimeTimeAffinityMultiplier = slimeTimeAffinityMultiplier;
+            TotalCountOfSimulationTicks = totalCountOfSimulationTicks;
+            TicksPerFrame = ticksPerFrame;
+            FrameRate = frameRate;
+            Seed = seed;
+            ThreadCount = threadCount;
+            SlimeAffinityRadius = slimeAffinityRadius;
+            SlimeOccurenceAffinityMultiplier = slimeOccurenceAffinityMultiplier;
+            SlimeTimeAffinityMultiplier = slimeTimeAffinityMultiplier;
         }
 
-        public string Seed => _seed;
-        public int TotalCountOfSimulationTicks => _totalCountOfSimulationTicks;
-        public int TicksPerFrame => _ticksPerFrame;
-        public int FrameRate => _frameRate;
-        public int SlimeAffinityRadius => _slimeAffinityRadius;
-        public int SlimeOccurenceAffinityMultiplier => _slimeOccurenceAffinityMultiplier;
-        public int SlimeTimeAffinityMultiplier => _slimeTimeAffinityMultiplier;
-        public int ThreadCount => _threadCount;
+        public string Seed { get; }
+        public int TotalCountOfSimulationTicks { get; }
+        public int TicksPerFrame { get; }
+        public int FrameRate { get; }
+        public int SlimeAffinityRadius { get; }
+        public int SlimeOccurenceAffinityMultiplier { get; }
+        public int SlimeTimeAffinityMultiplier { get; }
+        public int ThreadCount { get; }
 
         public static SimulationSettings Default() {
             return new();
@@ -52,12 +41,12 @@ namespace Slimulator {
         }
 
         public static SimulationSettings Benchmark() {
-            return new SimulationSettings(totalCountOfSimulationTicks: 30_000, ticksPerFrame: 1000, frameRate: 1,
+            return new (totalCountOfSimulationTicks: 30_000, ticksPerFrame: 1000, frameRate: 1,
                 threadCount: 1);
         }
 
         public static SimulationSettings QuickTest() {
-            return new SimulationSettings(totalCountOfSimulationTicks: 10_000, ticksPerFrame: 10, frameRate: 60);
+            return new (totalCountOfSimulationTicks: 10_000, ticksPerFrame: 10, frameRate: 60);
         }
     }
 }
